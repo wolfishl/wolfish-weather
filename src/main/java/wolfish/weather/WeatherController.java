d5f9bc5b6d7f6f7d5ebaec7e5a19b51c972d3b7c
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-05-03 19:55:40 -0400

    finished GUI

--- src/main/java/wolfish/weather/WeatherController.java
+++ src/main/java/wolfish/weather/WeatherController.java
@@ -2,95 +2,77 @@ package wolfish.weather;
 
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import wolfish.weather.OpenWeatherMapFeed;
 import wolfish.weather.OpenWeatherMapForecast;
 import wolfish.weather.OpenWeatherMapService;
 import wolfish.weather.OpenWeatherMapServiceFactory;
 
+import java.util.List;
+
 public class WeatherController {
     @FXML
     TextField locationTextField;
     @FXML
-    Label today;
-    @FXML
-    Label tomorrow;
-    @FXML
-    Label day2;
-    @FXML
-    Label day3;
-    @FXML
-    Label day4;
+    private List<Label> days;
     @FXML
     RadioButton farenheit;
     @FXML
     RadioButton celsius;
     @FXML
-    Label todayTemp;
+    private List<Label> temperature;
+
     @FXML
-    Label tomorrowTemp;
-    @FXML
-    Label day2Temp;
-    @FXML
-    Label day3Temp;
-    @FXML
-    Label day4Temp;
+    private List<ImageView> images;
+
 
     @FXML
     private void initialize() {
         ToggleGroup group = new ToggleGroup();
         farenheit.setToggleGroup(group);
         celsius.setToggleGroup(group);
+        farenheit.setSelected(true);
     }
 
 
     public void search(ActionEvent e)
     {
+
         String location = locationTextField.getText();
         String units = celsius.isSelected() ? "metric" : "imperial";
 
         OpenWeatherMapServiceFactory factory = new OpenWeatherMapServiceFactory();
         OpenWeatherMapService service = factory.newInstance();
 
-        Disposable disposableCurrent = service.getCurrentWeather(location, units).subscribeOn((Schedulers.io()))
-                .observeOn(Schedulers.trampoline())
-                .subscribe(this::onGetCurrentWeather,
-                        this::onError);
-
         Disposable disposableForecast = service.getWeatherForecast(location, units)
                 .subscribeOn((Schedulers.io()))
                 .observeOn(Schedulers.trampoline())
                 .subscribe(this::onGetForecast,
                         this::onError);
+ }
 
 
-        today.setText("Today");
-        tomorrow.setText("Tomorrow");
-        day2.setText("2 Days");
-        day3.setText("3 Days");
-        day4.setText("4 Days");
-
-
-    }
-
-    public void onGetCurrentWeather(OpenWeatherMapFeed feed){
-        
-
-    }
-
-    public OpenWeatherMapForecast.HourlyForecast[] onGetForecast(OpenWeatherMapForecast forecast)
+    public void onGetForecast(OpenWeatherMapForecast forecast)
     {
-        OpenWeatherMapForecast.HourlyForecast[] forecasts = new OpenWeatherMapForecast.HourlyForecast[4];
-        forecasts[0] = forecast.getForcastFor(1);
-        forecasts[1] = forecast.getForcastFor(2);
-        forecasts[2] = forecast.getForcastFor(3);
-        forecasts[3] = forecast.getForcastFor(4);
-        return forecasts;
+        Platform.runLater(new Runnable() {
+            @Override public void run() {
+
+                for (int index = 0; index < days.size(); index++)
+                {
+                    days.get(index).setText(forecast.getForcastFor(index).getDate() + "");
+                    temperature.get(index).setText(forecast.getForcastFor(index).main.temp + "");
+                    images.get(index).setImage(new Image(forecast.getForcastFor(index).weather.get(0).getIconUrl()));
+                }
+            }
+        });
     }
 
     public void onError(Throwable throwable)
---
+++ src/test/java/wolfish/weather/WeatherControllerTest.java
@@ -0,0 +1,7 @@
+package wolfish.weather;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherControllerTest {
+
+}
